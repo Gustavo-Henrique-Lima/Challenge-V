fc0a9bcca5a22d6cff8436893d30a319ad041d22
Gustavo-Henrique-Lima <gustavohgustavo@hotmail.com>
2025-09-06 17:25:48 -0300

    Initial commit.

---
+++ usersreader/src/main/java/com/gustavonascimento/usersreader/UsersreaderApplication.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.usersreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersreaderApplication.class, args);
+	}
+
+}
---
+++ usersreader/src/test/java/com/gustavonascimento/usersreader/UsersreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.usersreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
