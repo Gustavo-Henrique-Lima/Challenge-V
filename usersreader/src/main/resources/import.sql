9ed3c77e4fe49de63792b01921708cae42152546
Gustavo-Henrique-Lima <gustavohgustavo@hotmail.com>
2025-09-07 10:07:13 -0300

    feat: seed roles.

---
+++ usersreader/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_role (authority) VALUES ('manager');
+INSERT INTO tb_role (authority) VALUES ('analyst');
+INSERT INTO tb_role (authority) VALUES ('admin');
+INSERT INTO tb_role (authority) VALUES ('viewer');
